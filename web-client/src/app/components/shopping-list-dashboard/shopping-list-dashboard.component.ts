5f482c9554db26bf625fc31439d5474eac8a58cd
Carlos Espino Timón <carlosespinotimon@gmail.com>
2018-10-14 12:19:51 +0100

    Personal info improved

--- web-client/src/app/components/shopping-list-dashboard/shopping-list-dashboard.component.ts
+++ web-client/src/app/components/shopping-list-dashboard/shopping-list-dashboard.component.ts
@@ -12,11 +12,7 @@ import { startWith, map } from 'rxjs/operators';
 import { MatDialog, MatPaginator, MatTableDataSource } from '@angular/material';
 import { ArticleComponent } from '../article/article.component';
 
-export interface Transaction {
-    item: string;
-    cost: number;
-    units: number;
-  }
+
 
 @Component({
 	selector: 'app-shopping-list-dashboard',
@@ -30,48 +26,52 @@ export class ShoppingListDashboardComponent implements OnInit {
     subcategories: SubCategory[];
     shoppingList: [[Article, number]];
     cartArticles = new MatTableDataSource(this.shoppingList);
+    allArticles = new MatTableDataSource(this.articles);
     search: {} = {};
-    myControl = new FormControl();
-    filteredArticles: Observable<Article[]>;
     totalOfUnits: {[articleId: string]: number} = {};
     unitsToRemove: {[articleId: string]: number} = {};
     
 
-    displayedArticlesColumns: string[] = ['item', 'price', 'units', 'add'];
+    storedArticlesColumns: string[] = ['item', 'price', 'units', 'add'];
 
-    displayedColumns: string[] = ['item', 'cost', 'units', 'total', 'remove'];
+    listColumns: string[] = ['item', 'cost', 'units', 'total', 'remove'];
     
     @ViewChild(MatPaginator) paginator: MatPaginator;
 
+    
+	constructor(
+        private articlesService: ArticlesService,
+        private shoppingListService: ShoppingListService,
+        public dialog: MatDialog
+    ) { }
+    
+    openDialog(article: Article): void {
+        this.dialog.open(ArticleComponent, {
+            width: '850px',
+            data: article
+        });
+    }
+    
+    ngOnInit() {
+        this.getArticles();
+        this.allArticles.paginator = this.paginator;
+        this.getCategories();
+        this.getShoppingList();
+    }
+        
     /** Gets the total cost of all transactions. */
     getTotalCost() {
         return this.cartArticles.data.map(t => t[0].price * t[1]).reduce((acc, value) => acc + value, 0);
     }
 
-	constructor(
-		private articlesService: ArticlesService,
-        private shoppingListService: ShoppingListService,
-        public dialog: MatDialog
-    ) { }
-
-    openDialog(article: Article): void {
-        const dialogRef = this.dialog.open(ArticleComponent, {
-            width: '850px',
-            data: article
-        });
-      }
-
-	ngOnInit() {
-		this.getArticles();
-        this.getCategories();
-        this.getShoppingList();
-        // this.filteredArticles.paginator = this.paginator;
+    applyFilter(filterValue: string) {
+        this.allArticles.filter = filterValue.trim().toLocaleLowerCase();
+        
+        if (this.allArticles.paginator) {
+            this.allArticles.paginator.firstPage();
+        }
     }
-    
-    private _filter(value: string): Article[] {
-        const filterValue = value.toLocaleLowerCase();
-        return this.articles.filter(option => option.name.toLowerCase().includes(filterValue));
-    }
+
 
 	noArticles(): boolean {
 		let emptyArticles: Article[];
@@ -81,7 +81,7 @@ export class ShoppingListDashboardComponent implements OnInit {
 	getArticles() {
 		this.articlesService.getArticles().subscribe(data => {
             this.articles = data;
-            this.changeArticlesToShow();
+            this.allArticles.data = data;
 		});
 	};
 
@@ -109,17 +109,11 @@ export class ShoppingListDashboardComponent implements OnInit {
 		this.articlesService.getFilteredArticles(this.search)
             .subscribe(data => {
                 this.articles = data;
-                this.changeArticlesToShow();
+                this.allArticles.data = data;
             });
     };
     
-    changeArticlesToShow() {
-        this.filteredArticles = this.myControl.valueChanges
-                .pipe(
-                    startWith(''),
-                    map(value => this._filter(value))
-                );
-    }
+ 
 
 	getShoppingList() {
         this.shoppingList = this.shoppingListService.getShoppingList();
